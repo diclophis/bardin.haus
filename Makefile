2f6dfb5215fff374410dd78623b18067f0c1677a
Jon Bardin <diclophis@gmail.com>
2017-01-07 20:24:20 -0800

    for starting a new dev stack with kubeadm

---
+++ Makefile
@@ -0,0 +1,4 @@
+# Makefile for common tasks
+
+provision:
+	sh provision.sh
---
+++ provision.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -x
+set -e
+
+. ~/.bardin-haus-env.sh
+
+ansible-playbook -vvvv -i ansible/inventory/local ansible/provision-playbook.yml
+ansible-playbook -vvvv -i ansible/inventory/bardin-haus ansible/install-playbook.yml

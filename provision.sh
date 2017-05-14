e34d93818ad4151c2ba23d028aa3e5dbe2f44c9c
Jon Bardin <diclophis@gmail.com>
2017-05-13 23:43:38 -0700

    dont try to export external api

--- provision.sh
+++ provision.sh
@@ -5,5 +5,5 @@ set -e
 
 . ~/.bardin-haus-env.sh
 
-ansible-playbook -vvvv -i ansible/inventory/local ansible/provision-playbook.yml
+#ansible-playbook -vvvv -i ansible/inventory/local ansible/provision-playbook.yml
 ansible-playbook -vvvv -i ansible/inventory/bardin-haus ansible/install-playbook.yml
